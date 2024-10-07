11fc7124311e07656da4939e53939db3d36820d3
Piotr Meresinski <piotr.meresinski@gmail.com>
2024-10-07 22:00:59 +0200

    Wyświetlenie sumy produktów

--- Zadanie 3.2.py
+++ Zadanie 3.2.py
@@ -1,9 +1,11 @@
+print("Zadanie 1:")
 słownik = {
     "Piekarnia" : ["Chleb", "Pączek" , "Bułki"],
     "Warzywniak" : ["Marchew", "Seler", "Rukola"]
 }
-
-print(słownik)
+łączna_liczba_produktów = 0
 for i in słownik:
     produkty = słownik[i]
     print (f"Idę do {i} i kupuję tam: {produkty}")
+    łączna_liczba_produktów = łączna_liczba_produktów + len(produkty)
+print(f"W sumie kupuję {łączna_liczba_produktów} produktów.")
